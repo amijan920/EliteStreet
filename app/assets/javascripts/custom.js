7e09c3e07b7384f8c9090f37670530bcc742366e
Amiel Reveche <amielreveche@gmail.com>
2015-02-16 22:30:54 +0800

    change kpi to perspective

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+function ready() {
+	// $(".kpi-entry").on('click', function(e) {
+	// 	var i = $(this).data("dept");
+	// 	var j = $(this).data("index");
+
+	// 	$("#svg-"+i+"-"+j).slideToggle();
+	// });
+}
+
+$(document).ready(ready)
+$(document).on('page:load', ready)
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,44 +12,69 @@ class PageController < ApplicationController
 		@page = 'key'
 
 		require 'csv'
-		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-marketing_and_sales.csv');
-		@ms = CSV.read(@file_name, :headers => true).map do |row|
-		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
-		end
+		# @file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-marketing_and_sales.csv');
+		# @ms = CSV.read(@file_name, :headers => true).map do |row|
+		#   { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		# end
 		
-		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-product_development.csv');
-		@pd = CSV.read(@file_name, :headers => true).map do |row|
-		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
-		end
+		# @file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-product_development.csv');
+		# @pd = CSV.read(@file_name, :headers => true).map do |row|
+		#   { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		# end
 
-		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-production_and_delivery.csv');
-		@prd = CSV.read(@file_name, :headers => true).map do |row|
-		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
-		end
+		# @file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-production_and_delivery.csv');
+		# @prd = CSV.read(@file_name, :headers => true).map do |row|
+		#   { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		# end
 
-		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-externals_and_purchasing.csv');
-		@ext = CSV.read(@file_name, :headers => true).map do |row|
-		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
-		end
+		# @file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-externals_and_purchasing.csv');
+		# @ext = CSV.read(@file_name, :headers => true).map do |row|
+		#   { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		# end
 
-		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-finance.csv');
+		# @file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-finance.csv');
+		# @fin = CSV.read(@file_name, :headers => true).map do |row|
+		#   { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		# end
+
+		# @file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-human_resources.csv');
+		# @hr = CSV.read(@file_name, :headers => true).map do |row|
+		#   { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		# end
+
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-financials.csv');
 		@fin = CSV.read(@file_name, :headers => true).map do |row|
 		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
 		end
 
-		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-human_resources.csv');
-		@hr = CSV.read(@file_name, :headers => true).map do |row|
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-internal_operations.csv');
+		@int = CSV.read(@file_name, :headers => true).map do |row|
 		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
 		end
 
-		@kpis = 
-			[ {"dept" => "Marketing and Sales Department", "kpis" => @ms }, 
-				{"dept" => "Product Development Department", "kpis" => @pd },
-				{"dept" => "Production and Delivery Department", "kpis" => @prd },
-				{"dept" => "Externals and Purchasing Department", "kpis" => @ext },
-				{"dept" => "Finance Department", "kpis" => @fin },
-				{"dept" => "Human Resources Department", "kpis" => @hr } ];
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-customer.csv');
+		@cus = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
 
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-employee_growth.csv');
+		@emp = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+
+		# @kpis = 
+		# 	[ {"dept" => "Marketing and Sales Department", "kpis" => @ms }, 
+		# 		{"dept" => "Product Development Department", "kpis" => @pd },
+		# 		{"dept" => "Production and Delivery Department", "kpis" => @prd },
+		# 		{"dept" => "Externals and Purchasing Department", "kpis" => @ext },
+		# 		{"dept" => "Finance Department", "kpis" => @fin },
+		# 		{"dept" => "Human Resources Department", "kpis" => @hr } ];
+
+		@kpis = 
+			[ {"dept" => "Financial Perspective", "kpis" => @fin }, 
+				{"dept" => "Internal Business Processes Perspective", "kpis" => @int },
+				{"dept" => "Customer Perspective", "kpis" => @cus },
+				{"dept" => "Employee Growth Perspective", "kpis" => @emp } ];
 	end
 
 	def viewDashboard
