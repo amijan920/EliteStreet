4198da35eabb3c3b858019a1f7626b620e7d5489
Amiel Reveche <amielreveche@gmail.com>
2015-02-16 15:36:22 +0800

    KPI page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,6 +10,46 @@ class PageController < ApplicationController
 
 	def viewKeyIndicators
 		@page = 'key'
+
+		require 'csv'
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-marketing_and_sales.csv');
+		@ms = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+		
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-product_development.csv');
+		@pd = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-production_and_delivery.csv');
+		@prd = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-externals_and_purchasing.csv');
+		@ext = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-finance.csv');
+		@fin = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+
+		@file_name = File.join(Rails.root, 'app', 'assets/data', 'kpi-human_resources.csv');
+		@hr = CSV.read(@file_name, :headers => true).map do |row|
+		  { "Perspective" => row[0], "Measure" => row[1], "Initiatives" => row[2], "Good" => row[3], "Warning" => row[4], "Bad" => row[5]  }
+		end
+
+		@kpis = 
+			[ {"dept" => "Marketing and Sales Department", "kpis" => @ms }, 
+				{"dept" => "Product Development Department", "kpis" => @pd },
+				{"dept" => "Production and Delivery Department", "kpis" => @prd },
+				{"dept" => "Externals and Purchasing Department", "kpis" => @ext },
+				{"dept" => "Finance Department", "kpis" => @fin },
+				{"dept" => "Human Resources Department", "kpis" => @hr } ];
+
 	end
 
 	def viewDashboard
